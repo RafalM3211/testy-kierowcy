a8df1bf409d6d71782f7f3623fb70a0deacf70a1
RMalinowski <rafal.m3211@gmail.com>
2023-06-27 17:06:42 +0200

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HomeView from './components/views/Home';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <HomeView />
+  );
+}
+
+export default App;
