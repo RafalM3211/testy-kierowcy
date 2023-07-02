fe7fd2346765a14611698496add09018c35d549c
RMalinowski <rafal.m3211@gmail.com>
2023-07-03 00:34:23 +0200

    make basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Box } from "@mui/material";
 import HomeView from "./components/views/Home";
+import QuestionView from "./components/views/Question";
 import Header from "./components/patterns/Header/Header";
 import AppThemeProvider from "./context/theme/theme";
-import { Box } from "@mui/material";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<HomeView />} />
+      <Route path="/question" element={<QuestionView />} />
+    </>
+  )
+);
 
 function App() {
   return (
     <AppThemeProvider>
       <Box sx={{ minHeight: "100vh" }}>
         <Header />
-        <HomeView />
+        <RouterProvider router={router} />
       </Box>
     </AppThemeProvider>
   );
---
+++ src/components/views/Question.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export default function Question() {
+  return <Container>question</Container>;
+}
