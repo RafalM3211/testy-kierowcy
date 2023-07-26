fb0d260a846854fbb9eef5e78d5e14ebb0baadb8
RMalinowski <rafal.m3211@gmail.com>
2023-07-27 00:29:16 +0200

    make script for converting .wmv videos to .mp4

---
+++ scripts/conv.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Set the base directory path
+set "baseDir=C:\projekty\driving-licence-quiz\src\server\db\temporaryMedia"
+
+REM Loop through each file with wmv extension in the 'files' directory
+for %%F in ("%baseDir%\*.wmv") do (
+    REM Get the filename without extension
+    set "filename=%%~nF"
+
+    REM Build the output filename with mp4 extension
+    set "output=!baseDir!\!filename!.mp4"
+
+    REM Run ffmpeg command to convert the file
+    ffmpeg -i "%%F" "!output!"
+)
+
+echo Conversion of wmv files to mp4 completed
