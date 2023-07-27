3dd22613e288ec1830d59321dc0b7348a599ce50
RMalinowski <rafal.m3211@gmail.com>
2023-07-27 15:34:28 +0200

    fix bug that deletes all wmv files after converting first one and change target directory

--- scripts/conv.bat
+++ scripts/conv.bat
@@ -1,20 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Set the base directory path
-set "baseDir=C:\projekty\driving-licence-quiz\src\server\db\temporaryMedia"
+set "baseDir=C:\projekty\driving-licence-quiz\src\server\db\media"
 
-REM Loop through each file with wmv extension in the 'files' directory
 for %%F in ("%baseDir%\*.wmv") do (
-    REM Get the filename without extension
     set "filename=%%~nF"
-
-    REM Build the output filename with mp4 extension
     set "output=!baseDir!\!filename!.mp4"
-
-    REM Run ffmpeg command to convert the file
     ffmpeg -i "%%F" "!output!"
-    del "!baseDir!\*.wmv"
 )
+
+del "!baseDir!\*.wmv"
 
 echo Conversion of wmv files to mp4 completed
