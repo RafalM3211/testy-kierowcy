8ea7fa4c67f1eeda3db8e8792cb4b5b930ddde68
RMalinowski <rafal.m3211@gmail.com>
2023-08-13 20:00:07 +0200

    make dummy providers for stories and testing

---
+++ src/utility/dummyProviders/DummyProviders.tsx
@@ -0,0 +1,48 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import AppThemeProvider from "../../context/theme/theme";
+import { QuestionsProvider } from "../../context/AnseweredQuestions/AnseweredQuestions";
+import type { ReactNode, ComponentProps, JSX } from "react";
+
+const queryClient = new QueryClient();
+
+interface Props {
+  children: ReactNode;
+  routes?: ReactNode;
+}
+
+export function wrapInDummyProviders(Component: (props: any) => JSX.Element) {
+  type Props = ComponentProps<typeof Component>;
+
+  function Wrapped(props: Props) {
+    return (
+      <DummyProviders>
+        <Component {...props}></Component>
+      </DummyProviders>
+    );
+  }
+
+  return Wrapped;
+}
+
+export default function DummyProviders(props: Props) {
+  const routeElements = props.routes || (
+    <Route path="*" element={props.children} />
+  );
+  const router = createBrowserRouter(createRoutesFromElements(routeElements));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppThemeProvider>
+        <QuestionsProvider>
+          <RouterProvider router={router} />
+        </QuestionsProvider>
+      </AppThemeProvider>
+    </QueryClientProvider>
+  );
+}
