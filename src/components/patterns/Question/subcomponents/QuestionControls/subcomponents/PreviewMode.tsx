2d598663dc651503aeb4d2a1eb35b6c43fdafb2b
RMalinowski <rafal.m3211@gmail.com>
2023-09-26 19:17:47 +0200

    feature(questons counter): counter now displays real total questions count

--- src/components/patterns/Question/subcomponents/QuestionControls/subcomponents/PreviewMode.tsx
+++ src/components/patterns/Question/subcomponents/QuestionControls/subcomponents/PreviewMode.tsx
@@ -5,16 +5,18 @@ import { useAnsewersContext } from "../../../../../../context/Ansewers/Ansewers"
 import ButtonLink from "../../../../../atoms/ButtonLink/ButtonLink";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import { useEffect } from "react";
 
-function drawBasicQuestionCount(count: number) {
+function drawBasicQuestionCount(count: number, total: number) {
+  const basicQuestionsAmount = Math.min(total, 20);
   if (count > 20) return "20/20";
-  else return `${count}/20`;
+  else return `${count}/${basicQuestionsAmount}`;
 }
 
-function drawSpecializedQuestionCount(count: number) {
-  if (count > 20) return count - 20 + "/12";
-  else return "0/12";
+function drawSpecializedQuestionCount(count: number, total: number) {
+  const specializedQuestionsAmount = Math.max(total - 20, 0);
+  const specializedQuestionsCount = Math.max(count - 20, 0);
+
+  return `${specializedQuestionsCount}/${specializedQuestionsAmount}`;
 }
 
 export default function PreviewMode() {
@@ -38,12 +40,12 @@ export default function PreviewMode() {
       <Box sx={{ display: "flex", alignItems: "center", mb: "20px" }}>
         <QuestionCount
           label="Pytania podstawowe"
-          value={drawBasicQuestionCount(questionCount)}
+          value={drawBasicQuestionCount(questionCount, questionsAmount)}
           active={questionCount <= 20}
         />
         <QuestionCount
           label="Pytania specjalistyczne"
-          value={drawSpecializedQuestionCount(questionCount)}
+          value={drawSpecializedQuestionCount(questionCount, questionsAmount)}
           active={questionCount > 20}
         />
       </Box>
