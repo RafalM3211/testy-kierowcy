5a348526153c4b68ec90aa27325f1b4a520eb952
RMalinowski <rafal.m3211@gmail.com>
2023-12-27 15:32:49 +0100

    feature: add dropdown menu with logout button on user chip click

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,6 +9,7 @@ import Home from "./components/views/Home";
 import ExamQuestion from "./components/views/ExamQuestion/ExamQuestion";
 import PreviewQuestion from "./components/views/PreviewQuestion/PreviewQuestion";
 import Summary from "./components/views/Summary";
+import Login from "./components/views/Login/Login";
 import Error404 from "./components/views/errors/Error404";
 import Error500 from "./components/views/errors/Error500";
 import Header from "./components/patterns/Header/Header";
@@ -31,6 +32,7 @@ const router = createBrowserRouter(
         }
       >
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/summary" element={<Summary />} />
         <Route path="/question/:id" element={<PreviewQuestion />} />
       </Route>
@@ -58,7 +60,7 @@ export const mainMenuStructure: MenuItem[] = [
 ];
 
 export const userMenuStructure: MenuItem[] = [
-  { title: "wyloguj", to: "/" },
+  { title: "wyloguj", to: "/login" },
   { title: "costam", to: "/" },
 ];
 
--- src/components/patterns/Header/subcomponents/UserChip.tsx
+++ src/components/patterns/Header/subcomponents/UserChip.tsx
@@ -1,36 +1,99 @@
-import { Box, Typography, Avatar } from "@mui/material";
+import { Box, Typography, Avatar, Menu, MenuItem } from "@mui/material";
+import { useRef, useState } from "react";
 import type { SxProps } from "@mui/material";
+import ButtonLink from "../../../atoms/ButtonLink/ButtonLink";
+import { userMenuStructure } from "../../../../Router";
 
 interface Props {
   sx?: SxProps;
 }
 
 export default function UserChip(props: Props) {
+  const a = useRef();
+  const [isMenuOpen, setMenuOpen] = useState(false);
   const { sx } = props;
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        mx: "10px",
-        borderRadius: "50px",
-        bgcolor: "primary.light",
-        height: "min-content",
-        ...sx,
-      }}
-    >
-      <Typography
-        sx={(theme) => ({
-          ml: "10px",
-          mr: "5px",
-          color: "common.white",
-          textShadow: `1px 1px 2px ${theme.palette.grey[400]}`,
-        })}
+    <>
+      <Box
+        ref={a}
+        onClick={() => setMenuOpen(true)}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          mx: "10px",
+          borderRadius: "50px",
+          bgcolor: "primary.light",
+          height: "min-content",
+          cursor: "pointer",
+          transition: "all 0.2s",
+
+          "&:hover": {
+            bgcolor: "primary.main",
+          },
+
+          "&:hover .MuiAvatar-root ": {
+            bgcolor: "primary.light",
+          },
+          ...sx,
+        }}
       >
-        username
-      </Typography>
-      <Avatar sx={{ bgcolor: "primary.main" }}>U</Avatar>
-    </Box>
+        <Typography
+          sx={(theme) => ({
+            ml: "10px",
+            mr: "5px",
+            color: "common.white",
+            textShadow: `1px 1px 2px ${theme.palette.grey[400]}`,
+            userSelect: "none",
+          })}
+        >
+          username
+        </Typography>
+        <Avatar
+          sx={{
+            bgcolor: "primary.main",
+            transition: "all 0.2s",
+          }}
+        >
+          U
+        </Avatar>
+      </Box>
+      <Menu
+        onClose={() => setMenuOpen(false)}
+        open={a.current ? isMenuOpen : false}
+        anchorEl={a.current}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        sx={{ fontSize: "2em" }}
+      >
+        {userMenuStructure.map((item, index) => {
+          return (
+            <MenuItem
+              key={index}
+              onClick={() => setMenuOpen(false)}
+              sx={{
+                pr: "8px",
+                pt: "0px",
+                justifyContent: "center",
+              }}
+            >
+              <ButtonLink
+                size="small"
+                sx={{ color: "grey.700", fontSize: "1em" }}
+                to={item.to}
+              >
+                {item.title}
+              </ButtonLink>
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </>
   );
 }
